eb583fe0cd3be2b826c1d3a3601ba7e5f4462d19
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-09-26 20:20:38 +0700

    remove unused code

--- pdfinject/model.go
+++
@@ -1,9 +0,0 @@
-/*
-  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
-*/
-
-package pdfinject
-
-// Form represents fields from the PDF form.
-// define in key value map.
-//type Form map[string]interface{}
